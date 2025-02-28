26f544199e7006e78aab755bcf391e44b3a975f3
sushant1424 <sushantshrestha184@gmail.com>
2025-02-28 16:53:08 +0545

    Pass a argument to render the templates

--- django_project/blog_app/views.py
+++ django_project/blog_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+contents = [
+  {
+    'author' : 'Sushant',
+    'title' : 'Python Programming',
+    'body' : 'This a blog about python',
+    'date' : '24 jan'
+  },
+  {
+    'author' : 'Bisakha',
+    'title' : 'Java Programming',
+    'body' : 'This a blog about java',
+    'date' : '24 feb'
+  }
+]
+def home(request):
+  content = {
+    'posts' : contents
+  }
+  return render(request,'blog_app/index.html',content)
+
+def about(request):
+  return render(request,'blog_app/about.html',{'title':'About'})
+
